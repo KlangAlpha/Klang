5b1ef2f49901598db6ff600fc66382d243c62069
asmcos <asmcos@gmail.com>
2021-04-26 13:57:43 +0800

    增加setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+ Klang setup file
+"""
+
+from setuptools import setup
+
+setup(
+    name="Klang",
+    version="0.1.0",
+    url="https://github.com/asmcos/Klang",
+    author="asmcos",
+    author_email="asmcos@jeapedu.com",
+    packages=["Klang"],
+)
